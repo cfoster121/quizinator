e4d98154121c4e1012d868f4d1631e0df8d606f3
antieatingactivist <antieatingactivist@gmail.com>
2022-02-12 20:27:18 -0800

    correct answers turn green, wrong turn red

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -6,7 +6,7 @@ for (let i = 0; i < question.length; i++) {
 
     
     
-    var choiceListener = function (event) {
+    var choiceListener = function(event) {
         
         if (event.target.classList.contains("choice")) {
             const correct = question[i].querySelector("#correct");
@@ -17,18 +17,14 @@ for (let i = 0; i < question.length; i++) {
                 choice.classList.add("hover:bg-slate-300");
             }
             console.log(event.target);
-            // console.log(i, event.target.id);
-            // console.log(event.target.dataset.correct);
             event.target.classList.add("bg-slate-500");
             event.target.classList.add("selected");
             event.target.classList.remove("hover:bg-slate-300");
             if (event.target.id === event.target.dataset.correct) {
-                // correct.textContent = "correct!";
                 question[i].classList.add("correct");
                 question[i].classList.remove("incorrect");
 
             } else {
-                // correct.textContent = "wrong!";
                 question[i].classList.add("incorrect");
                 question[i].classList.remove("correct");
             }
@@ -44,18 +40,26 @@ showAnswers.addEventListener("click", function(event) {
     
     console.log(event);
     for (let i = 0; i < question.length; i++) {
+
         question[i].removeEventListener("click", choiceListener);
+
         const choices = question[i].getElementsByClassName("choice");
-        // console.log(question[i].classList.contains("correct"));
-        // console.log(choices);
+        
         for (let choice of choices) {
+            if (choice.id === choice.dataset.correct) {
+                choice.classList.add("bg-lime-500");
+            }
+            
             choice.classList.remove("hover:bg-slate-300");
             console.log(choice);
             if (choice.classList.contains("selected") && question[i].classList.contains("correct")) {
-                choice.classList.add("bg-lime-500");
+                choice.classList.add("bg-lime-500"); //will be unneccesary
             }
             if (choice.classList.contains("selected") && question[i].classList.contains("incorrect")) {
+                
                 choice.classList.add("bg-red-500");
+             
+                
             }
         }
 
