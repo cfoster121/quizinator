fa845559ee674dd28524abc6a3762966bc97fe07
antieatingactivist <antieatingactivist@gmail.com>
2022-02-12 19:38:23 -0800

    changes shade of color when selected

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -7,13 +7,25 @@ for (let i = 0; i < question.length; i++) {
         
         if (event.target.classList.contains("choice")) {
             const correct = question[i].querySelector("#correct");
-            
-            console.log(i, event.target.id);
-            console.log(event.target.dataset.correct);
+            const choices = event.target.parentNode.getElementsByClassName("choice");
+            for (let choice of choices) {
+                choice.classList.remove("bg-slate-500");
+                choice.classList.add("hover:bg-slate-500");
+            }
+            console.log(event.target);
+            // console.log(i, event.target.id);
+            // console.log(event.target.dataset.correct);
+            event.target.classList.add("bg-slate-500");
+            event.target.classList.remove("hover:bg-slate-300");
             if (event.target.id === event.target.dataset.correct) {
-                correct.textContent = "correct!";
+                // correct.textContent = "correct!";
+                question[i].classList.add("yes");
+                question[i].classList.remove("no");
+
             } else {
-                correct.textContent = "wrong!";
+                // correct.textContent = "wrong!";
+                question[i].classList.add("no");
+                question[i].classList.remove("yes");
             }
             
         }
