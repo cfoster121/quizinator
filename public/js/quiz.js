831c8ddaadb76307ec9415a08a3cbe3097d97d9a
antieatingactivist <antieatingactivist@gmail.com>
2022-02-12 20:10:59 -0800

    removed event listeners when quiz is completed

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,34 +1,63 @@
 const question = document.getElementsByClassName("question");
+const showAnswers = document.getElementById("show_answers");
 
 for (let i = 0; i < question.length; i++) {
 
 
-    question[i].addEventListener("click", function (event) {
+    
+    
+    var choiceListener = function (event) {
         
         if (event.target.classList.contains("choice")) {
             const correct = question[i].querySelector("#correct");
             const choices = event.target.parentNode.getElementsByClassName("choice");
             for (let choice of choices) {
                 choice.classList.remove("bg-slate-500");
-                choice.classList.add("hover:bg-slate-500");
+                choice.classList.remove("selected");
+                choice.classList.add("hover:bg-slate-300");
             }
             console.log(event.target);
             // console.log(i, event.target.id);
             // console.log(event.target.dataset.correct);
             event.target.classList.add("bg-slate-500");
+            event.target.classList.add("selected");
             event.target.classList.remove("hover:bg-slate-300");
             if (event.target.id === event.target.dataset.correct) {
                 // correct.textContent = "correct!";
-                question[i].classList.add("yes");
-                question[i].classList.remove("no");
+                question[i].classList.add("correct");
+                question[i].classList.remove("incorrect");
 
             } else {
                 // correct.textContent = "wrong!";
-                question[i].classList.add("no");
-                question[i].classList.remove("yes");
+                question[i].classList.add("incorrect");
+                question[i].classList.remove("correct");
             }
             
         }
      
-    })
+    }
+    question[i].addEventListener("click", choiceListener);
 }
+
+
+showAnswers.addEventListener("click", function(event) {
+    
+    console.log(event);
+    for (let i = 0; i < question.length; i++) {
+        question[i].removeEventListener("click", choiceListener);
+        const choices = question[i].getElementsByClassName("choice");
+        // console.log(question[i].classList.contains("correct"));
+        // console.log(choices);
+        for (let choice of choices) {
+            choice.classList.remove("hover:bg-slate-300");
+            console.log(choice);
+            if (choice.classList.contains("selected") && question[i].classList.contains("correct")) {
+                choice.classList.add("bg-lime-500");
+            }
+            if (choice.classList.contains("selected") && question[i].classList.contains("incorrect")) {
+                choice.classList.add("bg-red-500");
+            }
+        }
+
+    }
+});
